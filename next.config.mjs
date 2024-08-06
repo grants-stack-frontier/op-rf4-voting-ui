e6320120f5075ef817d742acf12a6f1a906b09ab
bitbeckers <code@bitbeckers.com>
2024-08-07 00:08:24 +0200

    fix(gha): remove standalone add sharp

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding", {
@@ -7,7 +8,6 @@ const nextConfig = {
     });
     return config;
   },
-  output: 'standalone',
   images: {
     remotePatterns: [
       {
