f8493fffe474863a9e0d4f007b9f1f4e71459d1c
bitbeckers <code@bitbeckers.com>
2024-08-15 12:00:01 +0200

    fix(api): update with latest spec

--- orval.config.ts
+++ orval.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'orval';
+import {defineConfig} from 'orval';
 
 export default defineConfig({
     "op-agora": {
         input: 'https://vote.optimism.io/api/v1/spec',
-        output: './src/__generated__/api/agora.ts',
+        output: {
+            mode: 'split',
+            target: './src/__generated__/api/agora.ts',
+            mock: true
+        },
         hooks: {
             afterAllFilesWrite: 'prettier --write',
         },
