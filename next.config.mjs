593de273a318eac70fe452857c6345a8c62369f9
nitegeist <nitegeist.dev@gmail.com>
2024-08-30 09:14:13 -0500

    add image url

--- next.config.mjs
+++ next.config.mjs
@@ -53,6 +53,12 @@ const nextConfig = {
 			},
 			{
 				protocol: 'https',
+				hostname: 'storage.googleapis.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
 				hostname: 'i.imgur.com',
 				port: '',
 				pathname: '/**',
