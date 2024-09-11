6b8ec2fa72ce003f01f02e5aeab331b7813485e1
jipstavenuiter <jipstavenuiter@gmail.com>
2024-09-11 12:16:20 +0200

    fix: remove time left to vote banner

--- src/components/common/callouts.tsx
+++ src/components/common/callouts.tsx
@@ -1,22 +1,22 @@
 "use client";
-import { votingEndDate } from "@/config";
 import { useIsBadgeholder } from "@/hooks/useIsBadgeholder";
 import { useAccount } from "wagmi";
-import { useVotingTimeLeft } from "../voting-ends-in";
+import { votingEndDate } from "@/config";
+// import { useVotingTimeLeft } from "../voting-ends-in";
 
 export function Callouts() {
   const { address, status } = useAccount();
   const isBadgeholder = useIsBadgeholder();
 
-  const [days, hours, minutes, seconds] = useVotingTimeLeft(votingEndDate);
+  // const [days, hours, minutes, seconds] = useVotingTimeLeft(votingEndDate);
 
-  if (Number(seconds) < 0) {
-    return (
-      <div className="bg-accent-foreground dark:text-black text-center p-3 text-white">
-        Voting in Retro Funding Round 4 has closed
-      </div>
-    );
-  }
+  // if (Number(seconds) < 0) {
+  //   return (
+  //     <div className="bg-accent-foreground dark:text-black text-center p-3 text-white">
+  //       Voting in Retro Funding Round 4 has closed
+  //     </div>
+  //   );
+  // }
 
   if (!address) return null;
   if (["connecting", "reconnecting"].includes(status)) return null;
