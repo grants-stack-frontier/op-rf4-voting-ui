72d23f3b73f8f5160f869788d4d588805ced174e
jipstavenuiter <jipstavenuiter@gmail.com>
2024-09-11 12:58:23 +0200

    fix: add voting warning again with updates dates

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const agoraRoundsAPI = "/api/agora/retrofunding/rounds/5";
 
-export const votingEndDate = new Date(Date.UTC(2024, 6, 11, 19, 0, 0));
+export const votingEndDate = new Date(Date.UTC(2024, 9, 5, 0, 0, 0));
 
 export const badgeholderManualUrl =
   "https://gov.optimism.io/t/retro-funding-4-badgeholder-manual/8239";
